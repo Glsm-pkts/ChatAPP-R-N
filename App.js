abfa00b94788cc7ef16ae4918987f4eb5dedc17d
Büşra Gülsüm PEKTAŞ <158344482+Glsm-pkts@users.noreply.github.com>
2024-10-22 21:20:56 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-get-random-values';
+
+import React, { useEffect } from "react";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ChatList from "./screens/ChatList";
+import Settings from "./screens/Settings";
+import Chat from "./screens/Chat";
+import SignUp from "./screens/SignUp";
+import SignIn from "./screens/SignIn";
+import { Ionicons } from "@expo/vector-icons";
+import { Provider as PaperProvider } from "react-native-paper";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import 'react-native-get-random-values';
+import { getFirestore } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBiThCsyVC8AK1tXUVd_I8PQ3DxJpseBgY",
+  authDomain: "webbb-2391a.firebaseapp.com",
+  projectId: "webbb-2391a",
+  storageBucket: "webbb-2391a.appspot.com",
+  messagingSenderId: "343520627227",
+  appId: "1:343520627227:web:b62618d27e25d324a846be",
+  measurementId: "G-XV4JC1XFH0"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const auth = getAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
+const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+const TabsNavigator = () => {
+  const navigation = useNavigation();
+
+  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigation.navigate("SignUp");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [navigation]);
+
+  return (
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          return (
+            <Ionicons
+              name={route.name === "ChatList" ? "chatbubbles" : "settings"}
+              color={color}
+              size={size}
+            />
+          );
+        },
+      })}
+    >
+      <Tabs.Screen name="ChatList" component={ChatList} />
+      <Tabs.Screen name="Settings" component={Settings} />
+    </Tabs.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <PaperProvider>
+        <Stack.Navigator >
+          <Stack.Screen
+            name="Main"
+            component={TabsNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Chat" component={Chat} />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{ presentation: "fullScreenModal" }}
+          />
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ presentation: "fullScreenModal" }}
+          />
+        </Stack.Navigator>
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
